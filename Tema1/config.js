109385322c64696e23016a7bde0423c307eafcaa
HOOOD1N1 <44617359+HOOOD1N1@users.noreply.github.com>
2022-03-21 22:32:08 +0200

    Add document

--- Tema1/config.js
+++ Tema1/config.js
@@ -1,10 +1,10 @@
 const config = {
   protocol: "TCP",
   address: "127.0.0.1",
-  port: 5004,
+  port: 3008,
   gbSent: 1,
   bytesInGb: 1_000_000_000,
-  messageSize: 32_203,
+  messageSize: 47504,
 };
 
 export default config;
